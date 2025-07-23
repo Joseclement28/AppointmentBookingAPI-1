e7b36fe26db8c160fb3228643061002434e91737
Joseclement28 <joseclement2807@gmail.com>
2025-07-23 11:40:30 +0530

    first commit

---
+++ src/main/java/com/AppointmentBooking/AppointmentBookingApi1Application.java
@@ -0,0 +1,14 @@
+package com.AppointmentBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentBookingApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentBookingApi1Application.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/AppointmentBooking/AppointmentBookingApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppointmentBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentBookingApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
